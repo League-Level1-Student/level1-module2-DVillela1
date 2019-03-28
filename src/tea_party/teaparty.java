4fd9dfd8d943c29547d8fbd4cfb506e25c9e405e
league <league@wts13.attlocal.net>
2019-03-27 17:42:31 -0700

    teaparty

--- src/tea_party/teaparty.java
+++ src/tea_party/teaparty.java
@@ -1,5 +1,29 @@
 package tea_party;
 
 public class teaparty {
-
+	private String name;
+	private boolean isWoman;
+	private boolean isKnighted;
+	public String welcome(String name, boolean isWoman, boolean isKnighted)
+    {
+		String x;
+		if (isWoman && isKnighted) {
+			x = "Lady";
+		return "Hello " + x + " " + name;
+		}
+		if (isWoman && isKnighted == false) {
+			x = "Ms.";
+		return "Hello" + x + " " + name;
+		}
+		if (isWoman == false && isKnighted) {
+			x = "Sir";
+		return "Hello" + x + " " + name;
+		}
+		if (isWoman == false && isKnighted == false) {
+			x = "Mr.";
+		return "Hello" + x + " " + name;
+		}
+		return "ok";
+    }
+	
 }
